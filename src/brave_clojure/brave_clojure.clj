562158d6c2a1709351b0eb3ae473d0e6bc76c02e
Daniel Horrisberger <dhorri@mecon.gob.ar>
2023-09-25 10:08:42 -0300

    remove invocation

--- src/brave_clojure/brave_clojure.clj
+++ src/brave_clojure/brave_clojure.clj
@@ -3,7 +3,6 @@
   (:require [clojure.string :as string]))
 
 (def filename "resources/suspects.csv")
-(slurp filename)
 
 (defn parse [string]
   (map #(string/split % #",") (string/split string #"\n")))
