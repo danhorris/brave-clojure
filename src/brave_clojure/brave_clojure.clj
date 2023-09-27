9b3122b02f1f188971240453e61d7dcdcbbef369
Daniel Horrisberger <dhorri@mecon.gob.ar>
2023-09-27 09:04:02 -0300

    finished example with comments

--- src/brave_clojure/brave_clojure.clj
+++ src/brave_clojure/brave_clojure.clj
@@ -8,3 +8,31 @@
   (map #(string/split % #",") (string/split filename-string #"\n")))
 
 (parse (slurp filename))
+
+(def map-keys [:name :age])
+
+(defn str->int
+  [str]
+  (Integer. str))
+
+;assoc a function to apply for a key
+(def conversions {:name identity
+                  :age str->int})
+
+; apply the function for a key
+(defn convert
+  [age-key value]
+  ((get conversions age-key) value))
+
+
+(defn mapify
+  "Return a seq of maps like {:name \"Edward Cullen\" :age 10}"
+  [rows]
+  (map (fn [unmapped-row]
+         (reduce (fn [row-map [key value]]
+                   (assoc row-map key (convert key value)))
+                 {}
+                 (map vector map-keys unmapped-row)))
+       rows))
+
+(mapify (parse (slurp filename)))
